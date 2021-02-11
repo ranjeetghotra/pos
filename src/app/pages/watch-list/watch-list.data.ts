b1980b8bd4fea63fa4bd11a7955f9d13fba9263b
ranjeetghotra <ranjeetghotra6@gmail.com>
2021-02-11 16:13:50 +0530

    watch list page updated

--- src/app/pages/watch-list/watch-list.data.ts
+++ src/app/pages/watch-list/watch-list.data.ts
@@ -55,4 +55,74 @@ export const WatchList = [
       isRise: true
     }
   },
+  {
+    title: 'GGL',
+    subTitle: 'Google, Inc',
+    rank1: {
+      value: '33.35',
+      percentage: '0.87%',
+      isRise: false
+    },
+    rank2: {
+      value: '33.35',
+      percentage: '0.87%',
+      isRise: true
+    }
+  },
+  {
+    title: 'TSLA',
+    subTitle: 'Tesla, Inc',
+    rank1: {
+      value: '1,607.95',
+      percentage: '0.91%',
+      isRise: false
+    },
+    rank2: {
+      value: '1,607.95',
+      percentage: '0.91%',
+      isRise: false
+    }
+  },
+  {
+    title: 'APLE',
+    subTitle: 'Apple, Inc',
+    rank1: {
+      value: '162.50',
+      percentage: '0.88%',
+      isRise: false
+    },
+    rank2: {
+      value: '162.50',
+      percentage: '0.88%',
+      isRise: true
+    }
+  },
+  {
+    title: 'SMNG',
+    subTitle: 'Samsung, Inc',
+    rank1: {
+      value: '108.22',
+      percentage: '1.33%',
+      isRise: true
+    },
+    rank2: {
+      value: '108.22',
+      percentage: '1.33%',
+      isRise: false
+    }
+  },
+  {
+    title: 'HUWE',
+    subTitle: 'Huawei, Inc',
+    rank1: {
+      value: '1,607.95',
+      percentage: '0.91%',
+      isRise: false
+    },
+    rank2: {
+      value: '1,607.95',
+      percentage: '0.91%',
+      isRise: false
+    }
+  },
 ];
